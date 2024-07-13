6e1c27a17c61d6d869306adc3d2b5d09122b8884
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-13 17:12:49 -0300

    [ :door: ] | implements: gateway - sse > subscribe (module::application)

--- lib/modules/application/gateway/http/sse/gateway.dart
+++ lib/modules/application/gateway/http/sse/gateway.dart
@@ -1,13 +1,26 @@
+import 'dart:convert';
+
+import 'package:mangueweb/modules/domain/gateway/http.dart';
 import 'package:mangueweb/modules/domain/gateway/sse.dart';
+import 'package:http/http.dart' as http;
 
 class NativeServerSentEventsGateway implements SSEGateway {
-  final String _url;
+  final HttpGateway _engine;
 
-  NativeServerSentEventsGateway(this._url);
+  NativeServerSentEventsGateway(this._engine);
 
   @override
-  Stream<String> subscribe(String path) {
-    // TODO: implement subscribe
-    throw UnimplementedError();
+  Stream<String> subscribe([String? path]) async* {
+    final response = await _engine.get(path, {
+      'Accept': 'text/event-stream',
+    }) as http.StreamedResponse;
+
+    if (response.statusCode == 200) {
+      await for (var data in response.stream.transform(utf8.decoder)) {
+        yield data;
+      }
+    } else {
+      throw Exception('Failed to connect to the server');
+    }
   }
 }
