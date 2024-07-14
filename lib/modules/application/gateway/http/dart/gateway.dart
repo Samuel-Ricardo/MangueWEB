48423c13d6867f6dba2893805781b77145e3eacc
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-13 22:54:39 -0300

    [ :door: ] | update: gateway - http (module::application)

--- lib/modules/application/gateway/http/dart/gateway.dart
+++ lib/modules/application/gateway/http/dart/gateway.dart
@@ -2,32 +2,50 @@ import 'package:http/http.dart' as http;
 import 'package:mangueweb/modules/domain/gateway/http.dart';
 
 class DartHttpGateway implements HttpGateway {
-  final Uri _url;
+  final String _url;
 
-  DartHttpGateway(String url) : _url = Uri.parse(url);
+  DartHttpGateway(this._url);
 
   @override
-  Future<http.Response> get() async {
-    return await http.get(_url);
+  Future<http.Response> get(
+      [String? path, Map<String, String>? headers]) async {
+    return await http.get(Uri.parse("$_url/${path ?? ""}"), headers: headers);
   }
 
   @override
-  Future<http.Response> post(Object data) async {
-    return await http.post(_url, body: data);
+  Future<http.Response> post(
+      {String? path,
+      required Object data,
+      Map<String, String>? headers}) async {
+    return await http.post(Uri.parse("$_url/${path ?? ""}"),
+        body: data, headers: headers);
   }
 
   @override
-  Future<http.Response> put(Object data) async {
-    return await http.put(_url, body: data);
+  Future<http.Response> put(
+      {String? path,
+      required Object data,
+      Map<String, String>? headers}) async {
+    return await http.put(Uri.parse("$_url/${path ?? ""}"),
+        body: data, headers: headers);
   }
 
   @override
-  Future<http.Response> patch(Object data) async {
-    return await http.patch(_url, body: data);
+  Future<http.Response> patch(
+      {String? path,
+      required Object data,
+      Map<String, String>? headers}) async {
+    return await http.patch(
+      Uri.parse("$_url/${path ?? ""}"),
+      body: data,
+      headers: headers,
+    );
   }
 
   @override
-  Future<http.Response> delete() async {
-    return await http.delete(_url);
+  Future<http.Response> delete(
+      [String? path, Map<String, String>? headers]) async {
+    return await http.delete(Uri.parse("$_url/${path ?? ""}"),
+        headers: headers);
   }
 }
--- lib/modules/domain/gateway/http.dart
+++ lib/modules/domain/gateway/http.dart
@@ -1,8 +1,10 @@
 abstract class HttpGateway {
-  Future<Object> get([String? url, Map<String, String>? headers]);
-  Future<Object> post([String? url, Object data, Map<String, String>? headers]);
-  Future<Object> put([String? url, Object data, Map<String, String>? headers]);
+  Future<Object> get([String? path, Map<String, String>? headers]);
+  Future<Object> post(
+      {String? path, required Object data, Map<String, String>? headers});
+  Future<Object> put(
+      {String? path, required Object data, Map<String, String>? headers});
   Future<Object> patch(
-      [String? url, Object data, Map<String, String>? headers]);
-  Future<Object> delete([String? url, Map<String, String>? headers]);
+      {String? path, required Object data, Map<String, String>? headers});
+  Future<Object> delete([String? path, Map<String, String>? headers]);
 }
