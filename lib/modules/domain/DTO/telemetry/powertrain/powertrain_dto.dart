b6048c619b3f3cdcd0dd09008224033097f568ee
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-16 11:10:14 -0300

    [ :label: ] | create: DTO - powertrain (module::domain::telemetry)

---
+++ lib/modules/domain/DTO/telemetry/powertrain/powertrain_dto.dart
@@ -0,0 +1,19 @@
+class PowertrainDTO {
+  final double speed;
+  final double rpm;
+  final _Temperature temperature;
+  final double? timestamp;
+
+  PowertrainDTO(
+      {required this.speed,
+      required this.rpm,
+      required this.temperature,
+      this.timestamp});
+}
+
+class _Temperature {
+  final double engine;
+  final double cvt;
+
+  _Temperature({required this.engine, required this.cvt});
+}
