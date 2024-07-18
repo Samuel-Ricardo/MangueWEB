8242252c4d68e3df761ab1d29ab6a1aed5941b17
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-18 16:48:32 -0300

    [ :factory: ] | compile: project (platform::web)

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Desktop\Desktop\Apps\flutter\flutter_windows_3.22.2-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Desktop\Desktop\Projects\flutter\MangueWEB"
+export "FLUTTER_ROOT=C:\Users\mangu\Downloads\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\mangu\projects\MangueWEB"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
