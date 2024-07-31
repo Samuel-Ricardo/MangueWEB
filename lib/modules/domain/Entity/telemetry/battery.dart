c43cf7e63f8619b4cfee3632a1bcf4a73c0142c1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-31 16:22:14 -0300

    [ :factory: ] | create: factory - entity > battery [fromJson] (module::domain)

--- lib/modules/domain/Entity/telemetry/battery.dart
+++ lib/modules/domain/Entity/telemetry/battery.dart
@@ -1,7 +1,25 @@
+import 'dart:convert';
+
 class Battery {
   final double voltage;
   final double stateOfCharge;
   final double? timestamp;
 
   Battery({required this.voltage, required this.stateOfCharge, this.timestamp});
+
+  /// Expect:
+  /// {
+  ///   "voltage": 100,
+  ///   "stateOfCharge": 100,
+  ///   "timestamp": 100
+  /// }
+  factory Battery.fromJson(String json) {
+    Map<String, dynamic> parsedJson = jsonDecode(json);
+
+    return Battery(
+      voltage: parsedJson['voltage'] as double,
+      stateOfCharge: parsedJson['stateOfCharge'] as double,
+      timestamp: parsedJson['timestamp'] as double?,
+    );
+  }
 }
