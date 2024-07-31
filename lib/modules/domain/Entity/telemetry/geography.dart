1d8b65a8b25e5c22d8f9adfd454bd35ec641bad4
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-31 17:06:56 -0300

    [ :sparkles: ] | create: entity - geography (module::domain::telemetry)

---
+++ lib/modules/domain/Entity/telemetry/geography.dart
@@ -0,0 +1,7 @@
+class Geography {
+  final double latitude;
+  final double longitude;
+  final double? timestamp;
+
+  Geography({required this.latitude, required this.longitude, this.timestamp});
+}
