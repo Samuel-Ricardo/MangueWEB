6c546bf577467353f539b3149ca5a9049ec3516b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-07-31 17:08:50 -0300

    [ :mount_fuji: ] | update: entity - geography [fromMap] (module::domain::telemetry)

--- lib/modules/domain/Entity/telemetry/geography.dart
+++ lib/modules/domain/Entity/telemetry/geography.dart
@@ -4,4 +4,12 @@ class Geography {
   final double? timestamp;
 
   Geography({required this.latitude, required this.longitude, this.timestamp});
+
+  factory Geography.fromMap(Map<String, dynamic> map) {
+    return Geography(
+      latitude: map['latitude'] as double,
+      longitude: map['longitude'] as double,
+      timestamp: map['timestamp'] as double?,
+    );
+  }
 }
